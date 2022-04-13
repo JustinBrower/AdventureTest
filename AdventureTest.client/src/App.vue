d177b76b29152ea6a5fccbcc0d2c8c603f94b712
Justin Brower <justin.lee.brower@gmail.com>
2022-04-13 14:40:43 -0600

    commit

--- AdventureTest.client/src/App.vue
+++ AdventureTest.client/src/App.vue
@@ -6,25 +6,76 @@
     <router-view />
   </main>
   <footer>
+    <!-- FIXME GET MUSIC TO WORK -->
+    <div
+      v-if="music"
+      class="mdi mdi-volume-high mdi-48px hoverable"
+      @click="flip"
+    ></div>
+    <div
+      v-if="!music"
+      class="mdi mdi-volume-off mdi-48px hoverable"
+      @click="flip"
+    ></div>
+    <audio autoplay :muted="!music" preload="auto">
+      <source src="src/assets/music/Hammerwatch.mp3" />
+    </audio>
     <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
+      Made with 💖 by A CodeWorks Student
     </div>
   </footer>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, onMounted, watchEffect } from 'vue'
 import { AppState } from './AppState'
+import { logger } from './utils/Logger'
+import Pop from './utils/Pop'
 export default {
   name: 'App',
   setup() {
+    let music = false
     return {
-      appState: computed(() => AppState)
+      music,
+      async flip() {
+        try {
+          music = !music
+          logger.log(music)
+        } catch (error) {
+          logger.error(error)
+          Pop.toast(error.message, 'error')
+        }
+      },
+      appState: computed(() => AppState),
+      music: computed(() => AppState.music)
     }
   }
 }
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
-
+.mdi-18px {
+  font-size: 18px;
+}
+.mdi-24px {
+  font-size: 24px;
+}
+.mdi-36px {
+  font-size: 36px;
+}
+.mdi-48px {
+  font-size: 48px;
+}
+.mdi-dark {
+  color: rgba(0, 0, 0, 0.54);
+}
+.mdi-dark.mdi-inactive {
+  color: rgba(0, 0, 0, 0.26);
+}
+.mdi-light {
+  color: rgba(255, 255, 255, 1);
+}
+.mdi-light.mdi-inactive {
+  color: rgba(255, 255, 255, 0.3);
+}
 </style>
--- AdventureTest.client/src/pages/HomePage.vue
+++ AdventureTest.client/src/pages/HomePage.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-8 d-flex justify-content-center">
+        <h1 class="pt-5">Adventure Test</h1>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-8 d-flex justify-content-center pt-5">
+        <button
+          style="width: 50vh; height: 8vh"
+          class="btn btn-dark rounded-pill hoverable"
+        >
+          <h1>New Game</h1>
+        </button>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-8 d-flex justify-content-center pt-5">
+        <button
+          style="width: 50vh; height: 8vh"
+          class="btn btn-dark rounded-pill hoverable"
+        >
+          <h1>Continue</h1>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -16,21 +35,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card{
-    width: 50vw;
-    > img{
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
 </style>
